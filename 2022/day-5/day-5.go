9a1f9233183e7592346a996f72aa847bf91a1545
Toby Law <toby.law@anz.com>
2022-12-05 19:59:01 +1100

    finished day 5

---
+++ 2022/day-5/day-5.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+const (
+	readStacks = iota
+	readMoves
+)
+
+type move struct {
+	src int
+	dst int
+	n   int
+}
+
+//go:embed sample
+var sample string
+
+//go:embed input
+var input string
+
+func pop(s string) (string, string) {
+	return s[:1], s[1:]
+}
+
+func chop(s string, n int) (string, string) {
+	return s[:n], s[n:]
+}
+
+func main() {
+	stacks := make([]string, 9)
+	var moves []move
+
+	state := readStacks
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		fmt.Println("current line", line)
+		switch state {
+		case readStacks:
+			if line == "" {
+				fmt.Println("changing state to read moves")
+				state = readMoves
+				break
+			}
+
+			tokens := strings.Split(line, "")
+			for i, t := range tokens {
+				switch t {
+				case " ", "[", "]":
+					continue
+				default:
+					index := (i - 1) / 4
+					stacks[index] += t
+				}
+			}
+		case readMoves:
+			var i, j, n int
+			fmt.Sscanf(line, "move %d from %d to %d", &n, &i, &j)
+			moves = append(moves, move{i - 1, j - 1, n})
+		}
+	}
+
+	for _, m := range moves {
+		// for i := 0; i < m.n; i++ {
+		// 	c, rest := pop(stacks[m.src])
+		// 	stacks[m.dst] = c + stacks[m.dst]
+		// 	stacks[m.src] = rest
+		// }
+
+		fmt.Println(m)
+		xs, rest := chop(stacks[m.src], m.n)
+		stacks[m.dst] = xs + stacks[m.dst]
+		stacks[m.src] = rest
+	}
+
+	for _, s := range stacks {
+		fmt.Print(strings.Split(s, "")[0])
+	}
+	fmt.Println()
+}
